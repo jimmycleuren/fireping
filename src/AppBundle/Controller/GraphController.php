ca297ba404403455b3ccfef2474579e870ea6221
jimmycleuren <jimmy.cleuren@gmail.com>
2017-06-09 10:20:20 +0200

    add smoke

--- src/AppBundle/Controller/GraphController.php
+++ src/AppBundle/Controller/GraphController.php
@@ -8,8 +8,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Device;
+use AppBundle\Entity\Probe;
 use AppBundle\Exception\RrdException;
 use AppBundle\Graph\PingGraph;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -52,6 +55,29 @@ class GraphController extends Controller
 
                 return $response;
             }
+        }
+
+        return new Response();
+    }
+
+    /**
+     * @param $id
+     * @return Response
+     *
+     * @Method("GET")
+     * @Route("/api/graphs/detail/{device_id}/{probe_id}")
+     * @ParamConverter("device", class="AppBundle:Device", options={"id" = "device_id"})
+     * @ParamConverter("probe", class="AppBundle:Probe", options={"id" = "probe_id"})
+     */
+    public function detailAction(Device $device, Probe $probe, Request $request)
+    {
+        $start = $request->get('start');
+        if ($probe->getType() == "ping") {
+            $filename = $this->get('graph.ping')->getDetailGraph($device, $probe, $start);
+            $response = new Response(file_get_contents($filename), 200);
+            $response->headers->set('Content-Type', 'image/png');
+
+            return $response;
         }
 
         return new Response();
--- src/AppBundle/Graph/PingGraph.php
+++ src/AppBundle/Graph/PingGraph.php
@@ -12,7 +12,7 @@ use AppBundle\Entity\Device;
 use AppBundle\Entity\Probe;
 use AppBundle\Exception\RrdException;
 
-class PingGraph extends Graph
+class PingGraph extends RrdGraph
 {
     public function getSummaryGraph(Device $device, Probe $probe)
     {
@@ -32,10 +32,75 @@ class PingGraph extends Graph
 
         $options[] = sprintf("DEF:%s=%s:%s:%s",'median', $this->storage->getFilePath($device, $probe), 'median', "AVERAGE");
         $options[] = sprintf("DEF:%s=%s:%s:%s",'loss', $this->storage->getFilePath($device, $probe), 'loss', "AVERAGE");
+        $options[] = "CDEF:dm0=median,0,100000,LIMIT";
+        $options[] = sprintf("CDEF:%s=%s,%s,%s",'loss_percent', "loss", "100", "*");
+        $this->calculateStdDev($options, $this->storage->getFilePath($device, $probe), $probe->getSamples());
+
+        $options[] = "CDEF:dmlow0=dm0,sdev0,2,/,-";
+        $options[] = "CDEF:s2d0=sdev0";
+        $options[] = sprintf("LINE:%s%s:%s", 'median', '#0000ff', 'median');
+        $options[] = sprintf("AREA:%s", 'dmlow0');
+        $options[] = "AREA:s2d0#0000FF44::STACK";
+
+        $options[] = "VDEF:avsd0=sdev0,AVERAGE";
+        $options[] = sprintf("GPRINT:%s:%s:%s", 'median', 'AVERAGE', "%7.2lf ms av md");
+        $options[] = sprintf("GPRINT:%s:%s:%s", 'loss_percent', 'AVERAGE', "%7.2lf %% av ls");
+        $options[] = sprintf("GPRINT:%s:%s", 'avsd0', "%7.2lf ms av sd");
+        $options[] = "COMMENT: \\n";
+        $options[] = "COMMENT:".date("D M j H\\\:i\\\:s Y")." \\r";
+
+        $return = rrd_graph($imageFile, $options);
+        $error = rrd_error();
+        if (!$return || $error) {
+            throw new RrdException($error);
+        }
+
+        return $imageFile;
+    }
+
+    public function getDetailGraph(Device $device, Probe $probe, $start = -3600)
+    {
+        $max = 100000;
+
+        if ($start < 0) {
+            $start = date("U") + $start;
+        }
+        $title = $device->getName();
+
+        $imageFile = tempnam("/tmp", 'image');
+        $options = array(
+            //"--slope-mode",
+            "--start", $start,
+            "--title=$title",
+            "--vertical-label=ms",
+            "--lower-limit=0",
+            "--width=600",
+        );
+
+        $options[] = sprintf("DEF:%s=%s:%s:%s",'median', $this->storage->getFilePath($device, $probe), 'median', "AVERAGE");
+        $options[] = sprintf("DEF:%s=%s:%s:%s",'loss', $this->storage->getFilePath($device, $probe), 'loss', "AVERAGE");
         $options[] = sprintf("VDEF:%s=%s,%s",'stdev', "median", "STDEV");
         $options[] = sprintf("CDEF:%s=%s,%s,%s",'loss_percent', "loss", "100", "*");
+        $options[] = "CDEF:lossred=loss,0.8,GT,median,UNKN,IF";
+        $options[] = "CDEF:lossorange=loss,0.4,GE,median,UNKN,IF";
+        $options[] = "CDEF:lossgreen=loss,0.4,LT,median,UNKN,IF";
 
-        $options[] = sprintf("%s:%s%s:%s", 'LINE', 'median', '#0000ff', 'median');
+        $total = $probe->getSamples();
+        $file = $this->storage->getFilePath($device, $probe);
+        for ($i = 1; $i <= $probe->getSamples(); $i++) {
+            $options[] = "DEF:ping$i=$file:ping$i:AVERAGE";
+            $options[] = "CDEF:cp$i=ping$i,$max,LT,ping$i,INF,IF";
+        }
+        for ($i = 1; $i <= $probe->getSamples(); $i++) {
+            $options[] = "CDEF:smoke$i=cp$i,UN,UNKN,cp$total,cp$i,-,IF";
+            $options[] = "AREA:cp$i";
+            $options[] = "STACK:smoke$i#33333322";
+        }
+
+        $options[] = sprintf("%s:%s%s:%s", 'LINE2', 'lossgreen', '#00ff00', '0');
+        $options[] = sprintf("%s:%s%s:%s", 'LINE2', 'lossorange', '#ff9900', '40%');
+        $options[] = sprintf("%s:%s%s:%s", 'LINE2', 'lossred', '#ff0000', '80%');
+
         $options[] = sprintf("GPRINT:%s:%s:%s", 'median', 'AVERAGE', "%7.2lf ms av md");
         $options[] = sprintf("GPRINT:%s:%s:%s", 'loss_percent', 'AVERAGE', "%7.2lf %% av ls");
         $options[] = sprintf("GPRINT:%s:%s", 'stdev', "%7.2lf ms av sd");
@@ -51,29 +116,24 @@ class PingGraph extends Graph
         return $imageFile;
     }
 
-    public function storeResult(Device $device, Probe $probe, $timestamp, $data)
+    private function calculateStdDev(&$options, $file, $pings)
     {
-        if (count($data) != $probe->getSamples()) {
-            throw new \Exception(count($data)." ping samples received, should have been ".$probe->getSamples());
-        }
-        $datasources = array();
-        $total = 0;
-        $failed = 0;
-        $success = 0;
+        $temp = array();
+        $temp2 = array();
+        $temp3 = array();
 
-        foreach ($data as $key => $result) {
-            $datasources['ping'.($key+1)] = $result;
-            if ($result != -1) {
-                $total += $result;
-                $success++;
-            } else {
-                $failed++;
+        for ($i = 1; $i < $pings; $i++) {
+            $options[] = "DEF:pin0p$i=$file:ping$i:AVERAGE";
+            $options[] = "CDEF:p0p$i=pin0p$i,UN,0,pin0p$i,IF";
+            if($i > 1) {
+                $temp[] = "p0p$i,UN,+";
+                $temp2[] = "p0p$i,+";
+                $temp3[] = "p0p$i,m0,-,DUP,*,+";
             }
         }
 
-        $datasources['loss'] = $failed / $probe->getSamples();
-        $datasources['median'] = $total / $success;
-
-        $this->storage->store($device, $probe, $timestamp, $datasources);
+        $options[] = "CDEF:pings0=$pings,p0p1,UN,".implode(",", $temp).",-";
+        $options[] = "CDEF:m0=p0p1,".implode(",", $temp2).",pings0,/";
+        $options[] = "CDEF:sdev0=p0p1,m0,-,DUP,*,".implode(",", $temp3).",pings0,/,SQRT";
     }
 }
