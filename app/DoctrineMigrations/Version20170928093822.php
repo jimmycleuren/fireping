a6b89ef6fa8cb61ce64b2650144755b5f47daf05
jimmycleuren <jimmy.cleuren@gmail.com>
2017-09-28 13:27:58 +0200

    slavegroups + initial gui

---
+++ app/DoctrineMigrations/Version20170928093822.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170928093822 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE device_slavegroups (device_id INT NOT NULL, slavegroup_id INT NOT NULL, INDEX IDX_C1AC34794A4C7D4 (device_id), INDEX IDX_C1AC347C29D09A3 (slavegroup_id), PRIMARY KEY(device_id, slavegroup_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE domain_slavegroups (domain_id INT NOT NULL, slavegroup_id INT NOT NULL, INDEX IDX_78BDA082115F0EE5 (domain_id), INDEX IDX_78BDA082C29D09A3 (slavegroup_id), PRIMARY KEY(domain_id, slavegroup_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE slave_group (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, secret VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_690F06A85E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE device_slavegroups ADD CONSTRAINT FK_C1AC34794A4C7D4 FOREIGN KEY (device_id) REFERENCES device (id)');
+        $this->addSql('ALTER TABLE device_slavegroups ADD CONSTRAINT FK_C1AC347C29D09A3 FOREIGN KEY (slavegroup_id) REFERENCES slave_group (id)');
+        $this->addSql('ALTER TABLE domain_slavegroups ADD CONSTRAINT FK_78BDA082115F0EE5 FOREIGN KEY (domain_id) REFERENCES domain (id)');
+        $this->addSql('ALTER TABLE domain_slavegroups ADD CONSTRAINT FK_78BDA082C29D09A3 FOREIGN KEY (slavegroup_id) REFERENCES slave_group (id)');
+        $this->addSql('DROP TABLE device_slaves');
+        $this->addSql('DROP TABLE domain_slaves');
+        $this->addSql('DROP INDEX UNIQ_408CF095E237E06 ON slave');
+        $this->addSql('ALTER TABLE slave ADD slavegroup_id INT DEFAULT NULL, DROP name, CHANGE id id VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE slave ADD CONSTRAINT FK_408CF09C29D09A3 FOREIGN KEY (slavegroup_id) REFERENCES slave_group (id)');
+        $this->addSql('CREATE INDEX IDX_408CF09C29D09A3 ON slave (slavegroup_id)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE device_slavegroups DROP FOREIGN KEY FK_C1AC347C29D09A3');
+        $this->addSql('ALTER TABLE domain_slavegroups DROP FOREIGN KEY FK_78BDA082C29D09A3');
+        $this->addSql('ALTER TABLE slave DROP FOREIGN KEY FK_408CF09C29D09A3');
+        $this->addSql('CREATE TABLE device_slaves (device_id INT NOT NULL, slave_id INT NOT NULL, INDEX IDX_B1C60E7B94A4C7D4 (device_id), INDEX IDX_B1C60E7B2B29BD08 (slave_id), PRIMARY KEY(device_id, slave_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE domain_slaves (domain_id INT NOT NULL, slave_id INT NOT NULL, INDEX IDX_DB573A9115F0EE5 (domain_id), INDEX IDX_DB573A92B29BD08 (slave_id), PRIMARY KEY(domain_id, slave_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE device_slaves ADD CONSTRAINT FK_B1C60E7B2B29BD08 FOREIGN KEY (slave_id) REFERENCES slave (id)');
+        $this->addSql('ALTER TABLE device_slaves ADD CONSTRAINT FK_B1C60E7B94A4C7D4 FOREIGN KEY (device_id) REFERENCES device (id)');
+        $this->addSql('ALTER TABLE domain_slaves ADD CONSTRAINT FK_DB573A9115F0EE5 FOREIGN KEY (domain_id) REFERENCES domain (id)');
+        $this->addSql('ALTER TABLE domain_slaves ADD CONSTRAINT FK_DB573A92B29BD08 FOREIGN KEY (slave_id) REFERENCES slave (id)');
+        $this->addSql('DROP TABLE device_slavegroups');
+        $this->addSql('DROP TABLE domain_slavegroups');
+        $this->addSql('DROP TABLE slave_group');
+        $this->addSql('DROP INDEX IDX_408CF09C29D09A3 ON slave');
+        $this->addSql('ALTER TABLE slave ADD name VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, DROP slavegroup_id, CHANGE id id INT AUTO_INCREMENT NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_408CF095E237E06 ON slave (name)');
+    }
+}
--- src/AppBundle/Controller/SlaveController.php
+++ src/AppBundle/Controller/SlaveController.php
@@ -21,26 +21,119 @@ use Symfony\Component\Routing\Annotation\Route;
 class SlaveController extends Controller
 {
     /**
+     * Lists all slave entities.
+     *
+     * @Route("/slaves", name="slave_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $slaves = $em->getRepository('AppBundle:Slave')->findAll();
+
+        return $this->render('slave/index.html.twig', array(
+            'slaves' => $slaves,
+            'active_menu' => 'slave',
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing slaveGroup entity.
+     *
+     * @Route("/slaves/{id}/edit", name="slave_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Slave $slave)
+    {
+        $deleteForm = $this->createDeleteForm($slave);
+        $editForm = $this->createForm('AppBundle\Form\SlaveType', $slave);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('slave_edit', array('id' => $slave->getId()));
+        }
+
+        return $this->render('slave/edit.html.twig', array(
+            'slaveGroup' => $slave,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+            'active_menu' => 'slave',
+        ));
+    }
+
+    /**
+     * Deletes a slave entity.
+     *
+     * @Route("/slaves/{id}", name="slave_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Slave $slave)
+    {
+        $form = $this->createDeleteForm($slave);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($slave);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('slave_index');
+    }
+
+    /**
+     * Creates a form to delete a slave entity.
+     *
+     * @param Slave $slave The slave entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Slave $slave)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('slave_delete', array('id' => $slave->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+            ;
+    }
+
+    /**
      * @param $id
      * @return JsonResponse
      *
      * @Method("GET")
      * @Route("/api/slaves/{id}/config")
-     * @ParamConverter("slave", class="AppBundle:Slave")
      */
-    public function configAction($slave, Request $request)
+    public function configAction($id, Request $request)
     {
-        $config = array();
+        $this->em = $this->container->get('doctrine')->getManager();
+        $slave = $this->em->getRepository("AppBundle:Slave")->findOneById($id);
 
-        foreach($slave->getDomains() as $domain) {
-            $this->getDomainDevices($domain, $config);
+        if (!$slave) {
+            $slave = new Slave();
+            $slave->setId($id);
         }
 
-        foreach($slave->getDevices() as $device) {
-            $result[$device->getName()] = array(
-                'ip' => $device->getIp(),
-                'probes' => $this->getDeviceProbes($device, $config),
-            );
+        $slave->setLastContact(new \DateTime());
+        $this->em->persist($slave);
+        $this->em->flush();
+
+        $config = array();
+
+        if ($slave->getSlaveGroup()) {
+            foreach ($slave->getSlaveGroup()->getDomains() as $domain) {
+                $this->getDomainDevices($domain, $config);
+            }
+
+            foreach ($slave->getSlaveGroup()->getDevices() as $device) {
+                $result[$device->getName()] = array(
+                    'ip' => $device->getIp(),
+                    'probes' => $this->getDeviceProbes($device, $config),
+                );
+            }
         }
 
         $response = new JsonResponse($config);
