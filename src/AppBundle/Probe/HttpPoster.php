4e2692970c27fa06c464e56e074947d4ec7d0979
Jimmy Cleuren <jimmy.cleuren@gmail.com>
2018-01-12 20:56:50 +0100

    remove obsolete code

--- src/AppBundle/Probe/HttpPoster.php
+++ src/AppBundle/Probe/HttpPoster.php
@@ -34,31 +34,4 @@ class HttpPoster extends Poster
         var_dump($response);
         return $response->getBody();
     }
-
-    public function postExperiment(Message $message)
-    {
-        $client = new Client();
-        try {
-            $response = $client->post($this->target, array(
-                'http_errors' => true,
-                'json' => json_encode($message->getBody()),
-            ));
-            return $response->getBody();
-        } catch (ClientException $exception) {
-            // Return|Throw -> Indicate Message SHOULD NOT be retried.
-            throw new DiscardMessageException(
-                $exception->getMessage(),
-                $exception->getCode(),
-                $exception);
-        } catch (ConnectException $exception) {
-            // Return|Throw -> Indicate Message SHOULD be retried
-            throw new RetryMessageException($exception->getMessage());
-        } catch (\Exception $exception) {
-            // Return|Throw -> Indicate an Exception that was not specifically handled.
-            // These SHOULD be discarded just to be safe (prevent queue blocking)
-            // This SHOULD raise an incident so we can investigate the exception,
-            //   and handle it specifically.
-            throw new UnhandledMessageException($exception->getMessage());
-        }
-    }
 }
