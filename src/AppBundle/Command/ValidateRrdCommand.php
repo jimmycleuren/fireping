dbd495ac214ece7a203fedc95b3bd18da2ef5491
Jimmy Cleuren <jimmy.cleuren@gmail.com>
2018-03-16 11:15:14 +0100

    ability to change rrd archives
    Signed-off-by: Jimmy Cleuren <jimmy.cleuren@gmail.com>

---
+++ src/AppBundle/Command/ValidateRrdCommand.php
@@ -0,0 +1,50 @@
+<?php
+namespace AppBundle\Command;
+
+use AppBundle\Storage\RrdStorage;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ValidateRrdCommand extends Command
+{
+    public function __construct(EntityManagerInterface $em, RrdStorage $storage)
+    {
+        $this->em = $em;
+        $this->storage = $storage;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('app:rrd:validate')
+            ->setDescription('Validate the existing rrd data and fix where needed');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $devices = $this->em->getRepository("AppBundle:Device")->findAll();
+
+        $progress = new ProgressBar($output);
+        $progress->start(count($devices));
+
+        foreach ($devices as $device) {
+            $probes = $device->getActiveProbes();
+            $slavegroups = $device->getActiveSlaveGroups();
+            foreach($probes as $probe) {
+                foreach ($slavegroups as $slavegroup) {
+                    $this->storage->validate($device, $probe, $slavegroup);
+                }
+            }
+
+            $progress->advance();
+        }
+
+        $progress->finish();
+        $output->writeln("");
+    }
+}
