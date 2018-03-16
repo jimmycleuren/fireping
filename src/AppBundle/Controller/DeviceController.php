3b510164eb9096f0ab295c7e62bd43ee8547adbb
Jimmy Cleuren <jimmy.cleuren@gmail.com>
2018-03-16 15:21:42 +0100

    improving the menu
    Signed-off-by: Jimmy Cleuren <jimmy.cleuren@gmail.com>

--- src/AppBundle/Controller/DeviceController.php
+++ src/AppBundle/Controller/DeviceController.php
@@ -10,19 +10,13 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class DeviceController extends Controller
 {
-    private $em = null;
-
     /**
      * @Route("/device/{id}")
      * @ParamConverter("device", class="AppBundle:Device")
      */
     public function getAction(Device $device, Request $request)
     {
-        $this->em = $this->container->get('doctrine')->getManager();
-        $domains = $this->em->getRepository("AppBundle:Domain")->findBy(array('parent' => null));
-
         return $this->render('device/view.html.twig', array(
-            'domains' => $domains,
             'device' => $device,
             'current_domain' => $device->getDomain()
         ));
