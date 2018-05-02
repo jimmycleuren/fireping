e46dd9645e707b234f2ea83819c98f554ebdbd7b
jimmycleuren <jimmy.cleuren@gmail.com>
2018-05-02 19:02:26 +0200

    upgrade to symfony 4.0
    Signed-off-by: Jimmy Cleuren <jimmy.cleuren@gmail.com>

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -30,7 +30,6 @@ class AppKernel extends Kernel
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
