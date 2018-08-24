e964ddf400b6f1e2c41b87dc6bddaf4c8fdf5864
jimmycleuren <jimmy.cleuren@gmail.com>
2018-08-24 19:35:30 +0200

    test fix

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,12 +4,11 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class IndexController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="home")
      */
     public function indexAction()
     {
