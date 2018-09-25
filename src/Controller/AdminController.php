559a9d61e21e9af9ccb0a125da7c960f64391a8d
rubench <ruben.charlier@gmail.com>
2018-09-25 11:56:56 +0200

    Add and edit user via Admin panel

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+
+class AdminController extends BaseAdminController
+{
+    public function createNewUserEntity()
+    {
+        return $this->get('fos_user.user_manager')->createUser();
+    }
+
+    public function persistUserEntity($user)
+    {
+        $this->get('fos_user.user_manager')->updateUser($user, false);
+        parent::persistEntity($user);
+    }
+
+    public function updateUserEntity($user)
+    {
+        $this->get('fos_user.user_manager')->updateUser($user, false);
+        parent::updateEntity($user);
+    }
+
+}
