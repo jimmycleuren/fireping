7b671271aeb50ca9b1f7ad66dc35f6b851151b8c
rubench <ruben.charlier@gmail.com>
2018-09-26 12:27:24 +0200

    Added role api when creating user

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,20 +6,13 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminContr
 
 class AdminController extends BaseAdminController
 {
-    public function createNewUserEntity()
-    {
-        return $this->get('fos_user.user_manager')->createUser();
-    }
-
     public function persistUserEntity($user)
     {
-        $this->get('fos_user.user_manager')->updateUser($user, false);
         parent::persistEntity($user);
     }
 
     public function updateUserEntity($user)
     {
-        $this->get('fos_user.user_manager')->updateUser($user, false);
         parent::updateEntity($user);
     }
 
