0e31e6a53ebb8dc8536c12c20c31f1174fb4c3b1
rubench <ruben.charlier@gmail.com>
2018-10-05 10:42:17 +0200

    Deleted unneeded use statements and variable

--- tests/App/Api/AbstractApiTest.php
+++ tests/App/Api/AbstractApiTest.php
@@ -3,20 +3,23 @@
 
 namespace App\Tests\App\Api;
 
-use Symfony\Component\BrowserKit\Cookie;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 abstract class AbstractApiTest extends WebTestCase
 {
-    protected $client = null;
+    protected $client;
 
     public function setUp()
     {
        $this->client = $this->createAuthorizedClient();
     }
 
-    protected function createAuthorizedClient()
+    /**
+     * Create an HTTP authorized client
+     *
+     * @return \Symfony\Bundle\FrameworkBundle\Client
+     */
+    protected function createAuthorizedClient(): \Symfony\Bundle\FrameworkBundle\Client
     {
         return static::createClient(array(), array(
             'PHP_AUTH_USER' => 'test',
