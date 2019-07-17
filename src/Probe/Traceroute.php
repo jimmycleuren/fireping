e110126c098bcba20af332f467dae7b9e1306911
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2019-07-18 01:10:41 +0200

    Move Traceroute to the Probe namespace and implement CommandInterface

--- src/Probe/Traceroute.php
+++ src/Probe/Traceroute.php
@@ -1,15 +1,12 @@
 <?php
-namespace App\DependencyInjection;
-use App\ShellCommand\PingShellCommand;
+declare(strict_types=1);
+
+namespace App\Probe;
+
+use App\ShellCommand\CommandInterface;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\Process\ExecutableFinder;
 
-
-/**
- * Class Traceroute
- * @package App\DependencyInjection
- */
-class Traceroute
+class Traceroute implements CommandInterface
 {
     private $maxHops = 30;
     private $delay = null;
@@ -18,25 +15,16 @@ class Traceroute
     private $targets = [];
     private $logger = null;
 
-    public function __construct($args, LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger)
     {
         $this->logger = $logger;
-        $this->delay = $args['delay_execution'];
-        $this->step = $args['step'];
-        $this->samples = $args['args']['samples'];
-        $this->targets = $args['targets'];
-
-        $finder = new ExecutableFinder();
-        if (!$finder->find("fping")) {
-            throw new \Exception("fping is not installed on this system.");
-        }
     }
 
-    public function execute()
+    public function execute(): array
     {
         usleep($this->delay * 1000);
 
-        $this->logger->debug("Launching traceroute (step=$this->step, samples=$this->samples) on ".json_encode($this->targets));
+        $this->logger->debug("Launching traceroute (step=$this->step, samples=$this->samples) on " . json_encode($this->targets));
 
         $ips = [];
         foreach ($this->targets as $target) {
@@ -50,7 +38,7 @@ class Traceroute
             $result[$target['id']] = $temp[$target['ip']];
         }
 
-        $this->logger->debug("Traceroute result: ".json_encode($result));
+        $this->logger->debug("Traceroute result: " . json_encode($result));
 
         return $result;
     }
@@ -60,7 +48,7 @@ class Traceroute
         $result = array();
         $active = array();
 
-        foreach($ips as $ip) {
+        foreach ($ips as $ip) {
             $active[$ip] = true;
         }
 
@@ -68,8 +56,8 @@ class Traceroute
         $start = microtime(true);
 
         //determine hops
-        for($i = 1; $i < $this->maxHops && count($active) > 0; $i++) {
-            $res = $this->exec("fping -H $i -C 1 ".implode(" ", array_keys($active))." 2>&1");
+        for ($i = 1; $i < $this->maxHops && count($active) > 0; $i++) {
+            $res = $this->exec("fping -H $i -C 1 " . implode(" ", array_keys($active)) . " 2>&1");
             $res = implode("\n", $res);
             //var_dump($res);
             foreach (array_keys($active) as $ip) {
@@ -89,14 +77,14 @@ class Traceroute
         $end = microtime(true);
         $remaining = $step - ($end - $start);
 
-        $this->logger->info("Trace took ".($end - $start)." seconds, ".$remaining." seconds remaining");
+        $this->logger->info("Trace took " . ($end - $start) . " seconds, " . $remaining . " seconds remaining");
 
         $waitTime = floor($remaining * 1000 / $samples);
 
         //ping all gathered hops in the remaining time
         $merged = array_unique($merged);
         $latencies = array();
-        $res = $this->exec("fping -C $samples -p ".($waitTime)." ".implode(" ", $merged)." 2>&1");
+        $res = $this->exec("fping -C $samples -p " . ($waitTime) . " " . implode(" ", $merged) . " 2>&1");
         $res = implode("\n", $res);
 
         foreach ($merged as $ip) {
@@ -122,5 +110,13 @@ class Traceroute
         exec($command, $out);
 
         return $out;
+    }
+
+    public function setArgs(array $args): void
+    {
+        $this->delay = $args['delay_execution'];
+        $this->step = $args['step'];
+        $this->samples = $args['args']['samples'];
+        $this->targets = $args['targets'];
     }
 }
