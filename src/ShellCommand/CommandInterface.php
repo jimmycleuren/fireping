96c9bdaf84fe7c1443bfc66e19e63e1a1cfbaea6
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2019-07-17 23:32:51 +0200

    Do not make the followed a required signature

--- src/ShellCommand/CommandInterface.php
+++ src/ShellCommand/CommandInterface.php
@@ -1,19 +1,10 @@
 <?php
+declare(strict_types=1);
 
 namespace App\ShellCommand;
 
-use Psr\Container\ContainerInterface;
-use Psr\Log\LoggerInterface;
-
-/**
- * Created by PhpStorm.
- * User: kevinr
- * Date: 9/06/2017
- * Time: 12:38
- */
 interface CommandInterface
 {
-    function __construct($args, LoggerInterface $logger, ContainerInterface $container);
     function execute();
     function build();
     function valid();
