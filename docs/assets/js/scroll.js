83fa70449c3fb52e2a9aacb7ca4c7d71c42c4d54
Cedric Meukens <cedric.meukens@cegeka.com>
2019-07-18 13:41:06 +0200

    smooth scroll

--- docs/assets/js/scroll.js
+++ docs/assets/js/scroll.js
@@ -1,7 +1,13 @@
 function scrollDownSmooth(duration, steps, stepsLeft) {
     if (stepsLeft == 0) return;
-    window.scrollBy(0, Math.max(document.documentElement.clientHeight, window.innerHeight || 0)/steps);
-    setTimeout(() => { scrollDown(duration, steps, stepsLeft-1) }, duration / steps);
+    window.scrollBy(0, Math.max(document.documentElement.clientHeight, window.outerHeight || 0)/steps);
+
+    if(stepsLeft > steps/2) duration-=1;
+    else duration+=1;
+
+    if (duration < 0) duration = 0;
+
+    setTimeout(() => { scrollDownSmooth(duration, steps, stepsLeft-1); }, duration / steps);
 }
 
 function scrollDown() {
