6d960d370f598d5826f7f137246a16edf9577fdf
Cedric Meukens <cedric.meukens@cegeka.com>
2019-07-18 13:06:45 +0200

    scroll button

--- docs/assets/js/scroll.js
+++ docs/assets/js/scroll.js
@@ -1,4 +1,6 @@
-function scrollDown(delay, steps) {
-    window.scrollBy(0, window.innerHeight/steps);
-    setTimeout(scrollDown(delay, steps-1), delay);
+function scrollDown(delay, steps, stepsLeft) {
+    if (stepsLeft == 0) return;
+    stepsLeft = stepsLeft || steps;
+    window.scrollBy(0, window.outerHeight/steps);
+    setTimeout(() => { scrollDown(delay, steps, stepsLeft-1) }, delay);
 }
