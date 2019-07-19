912d11d78844adda0405123f6c2f95f153aee9f9
Cedric Meukens <cedric.meukens@cegeka.com>
2019-07-19 14:10:28 +0200

    fixing scroll

--- docs/assets/js/scroll.js
+++ docs/assets/js/scroll.js
@@ -1,6 +1,7 @@
 function scrollDownSmooth(duration, steps, stepsLeft) {
     if (stepsLeft == 0) return;
-    window.scrollBy(0, Math.max(document.documentElement.clientHeight, window.outerHeight || 0)/steps);
+    let element = document.getElementsByClassName("article__header--overlay")[0];
+    window.scrollBy(0, element.clientHeight/steps);
 
     setTimeout(() => { scrollDownSmooth(duration, steps, stepsLeft-1); }, duration / steps);
 }
