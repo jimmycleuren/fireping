5d90c7e39bf4ae717e0178e71dfb5a5f32a2b180
Cedric Meukens <cedric.meukens@cegeka.com>
2019-07-22 09:44:35 +0200

    expand image

--- docs/assets/js/expand_img.js
+++ docs/assets/js/expand_img.js
@@ -1,19 +1,11 @@
 // Select all images
 let images = document.getElementsByTagName('img');
 
-// let overlay_div = document.createElement('div');
-// overlay_div.attributes.id = "overlay";
-// document.body.appendChild(overlay_div);
-
-// let close_btn = document.createElement('button');
-// close_btn.innerHTML = '<i class="fas fa-times"></i>';
-// overlay_div.appendChild(close_btn);
-
 for (var i = 0, count = images.length; i < count; ++i)
 {
-    let image = images[i]
+    images[i].attributes.onclick = "expandImage(" + images[i].src + ")";
+}
 
-    image.onclick(() => {
-        console.log(image.attributes.src);
-    });
+function expandImage(src) {
+
 }
