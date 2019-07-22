55363ba17ca5165b14fae57a5bad71e4337c2e6e
Cedric Meukens <cedric.meukens@cegeka.com>
2019-07-22 09:34:37 +0200

    Image overlay

--- docs/assets/js/expand_img.js
+++ docs/assets/js/expand_img.js
@@ -1,3 +1,16 @@
 // Select all images
-let images = document.getElementsByTagName("img");
-console.log(images);
+let images = document.getElementsByTagName('img');
+
+// let overlay_div = document.createElement('div');
+// overlay_div.attributes.id = "overlay";
+// document.body.appendChild(overlay_div);
+
+// let close_btn = document.createElement('button');
+// close_btn.innerHTML = '<i class="fas fa-times"></i>';
+// overlay_div.appendChild(close_btn);
+
+images.forEach((image) => {
+    image.onclick(() => {
+        console.log(image.attributes.src);
+    });
+});
