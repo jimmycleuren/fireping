5154efd4b5d4569a966ffe54c7f00208d68b469c
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2019-08-04 03:42:53 +0200

    Fix CommandFactory import

--- tests/App/Command/ProbeWorkerCommandTest.php
+++ tests/App/Command/ProbeWorkerCommandTest.php
@@ -3,7 +3,7 @@
 namespace Tests\App\Command;
 
 use App\Command\ProbeWorkerCommand;
-use App\ShellCommand\CommandFactory;
+use App\Probe\CommandFactory;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
