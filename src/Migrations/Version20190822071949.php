b9b4c5ff86646365c12b03683afa1f7a5ff0557c
kennyva <kenny.vanrusselt@cegeka.com>
2019-09-10 14:51:28 +0200

    made a whoopsy

--- src/Migrations/Version20190822071949.php
+++ src/Migrations/Version20190822071949.php
@@ -24,7 +24,6 @@ final class Version20190822071949 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, enabled TINYINT(1) NOT NULL, api_token VARCHAR(255) DEFAULT NULL, last_login DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), UNIQUE INDEX UNIQ_8D93D6497BA2F5EB (api_token), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('DROP TABLE fos_user');
     }
 
     public function postUp(Schema $schema): void
@@ -64,6 +63,8 @@ final class Version20190822071949 extends AbstractMigration
 
             $this->connection->insert('user', $data);
         }
+
+        $this->addSql('DROP TABLE fos_user');
 
     }
 
