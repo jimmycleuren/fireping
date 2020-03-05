fc34760131bddefcdc2e226cc648e2f899dfc45b
Kevin R <22906111+kevinreniers@users.noreply.github.com>
2020-03-05 15:22:27 +0100

    Remove echo statements from code (#142)
    * Remove echo statements from code
    
    These are being dumped to tests, which we don't want.
    
    * Add typehint and add back used functions.

--- src/Services/CleanupService.php
+++ src/Services/CleanupService.php
@@ -1,65 +1,63 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kennyva
- * Date: 10/10/2018
- * Time: 12:47
- */
 
 namespace App\Services;
 
 use App\Entity\Device;
+use App\Storage\RrdCachedStorage;
+use App\Storage\RrdDistributedStorage;
+use App\Storage\RrdStorage;
 use App\Storage\StorageFactory;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use function count;
 
-/**
- * Class CleanupService
- * @package App\Services
- */
 class CleanupService
 {
-
-    /** @var string */
+    /**
+     * @var string
+     */
     private $path;
-
-    /** @var EntityManagerInterface */
+    /**
+     * @var EntityManagerInterface
+     */
     private $em;
-
-    /** @var LoggerInterface */
+    /**
+     * @var LoggerInterface
+     */
     private $logger;
-
-    /** @var array */
+    /**
+     * @var array
+     */
     private $inactiveDevices;
-    /** @var array */
+    /**
+     * @var array
+     */
     private $activeDeviceIds;
-    /** @var array */
+    /**
+     * @var array
+     */
     private $storedDeviceIds;
-
-    /** @var array */
+    /**
+     * @var array
+     */
     private $storedActiveDevices;
-
-    /** @var array */
+    /**
+     * @var array
+     */
     private $activeProbes;
-    /** @var array */
+    /**
+     * @var array
+     */
     private $activeGroups;
-
-    /** @var \App\Storage\RrdCachedStorage|\App\Storage\RrdDistributedStorage|\App\Storage\RrdStorage  */
+    /**
+     * @var RrdCachedStorage|RrdDistributedStorage|RrdStorage
+     */
     private $storage;
 
-
-    /**
-     * CleanupService constructor.
-     * @param EntityManagerInterface $em
-     * @param LoggerInterface $logger
-     * @param ParameterBagInterface $params
-     * @param StorageFactory $storage
-     */
     public function __construct(EntityManagerInterface $em, LoggerInterface $logger, ParameterBagInterface $params, StorageFactory $storage)
     {
         $this->path = $params->get('rrd_storage_path');
-
         $this->em = $em;
         $this->logger = $logger;
         $this->storage = $storage->create();
@@ -72,27 +70,17 @@ class CleanupService
      */
     public function cleanup(): void
     {
-        echo PHP_EOL . 'Sitrep ' . PHP_EOL;
-
         $this->logger->info('Retrieving statistics...');
         $this->setCurrentSituation();
-
-        echo 'Remove Inactive Devices ' . PHP_EOL;
 
         $this->logger->info('Removing inactive devices...');
         $this->removeInactiveDevices();
 
-        echo 'Sitrep ' . PHP_EOL;
-
         $this->logger->info('Updating statistics...');
         $this->setCurrentSituation();
 
-        echo 'Remove Inactive Probes ' . PHP_EOL;
-
         $this->logger->info('Removing inactive probes...');
         $this->removeInactiveProbes();
-
-        echo 'Remove Inactive Slave Groups ' . PHP_EOL;
 
         $this->logger->info('Removing inactive slave groups...');
         $this->removeInactiveSlaveGroups();
@@ -104,12 +92,10 @@ class CleanupService
      */
     private function setCurrentSituation(): void
     {
-
         //create an array of existing directories
         $this->storedDeviceIds = $this->storage->listItems($this->path);
 
-        if($this->storedDeviceIds === null){
-            echo 'No devices listed, directory is either clean or wrongly set, exiting...' . PHP_EOL;
+        if ($this->storedDeviceIds === null) {
             $this->logger->info('No items found, directory is either clean or wrongly set');
             exit;
         }
@@ -124,21 +110,12 @@ class CleanupService
         //Do the comparison to get the inactive devices
         $this->activeDeviceIds = array_column($activeDevices, 'id');
         $this->inactiveDevices = array_diff($this->storedDeviceIds, $this->activeDeviceIds);
-
-        echo 'Stored Devices: ' . $this->getStoredDeviceCount() . PHP_EOL;
-        echo 'Active Devices: ' . $this->getActiveDeviceCount() . PHP_EOL;
-        echo 'Inactive Devices: ' . $this->getInactiveDeviceCount() . PHP_EOL;
     }
 
-
-    /**
-     * gathers the slave groups for all active devices
-     * that are stored on the filesystem
-     */
     protected function setActiveSlaveGroups(): void
     {
 
-        if($this->storedActiveDevices === null){
+        if ($this->storedActiveDevices === null) {
             return;
         }
 
@@ -147,67 +124,54 @@ class CleanupService
             $deviceId = $device->getId();
             $activeGroups = $device->getActiveSlaveGroups();
 
-            foreach ($activeGroups as $activeGroup){
+            foreach ($activeGroups as $activeGroup) {
                 $this->activeGroups[$deviceId][] = $activeGroup->getid() . '.rrd';
             }
 
-            if (isset($this->activeGroups[$deviceId]))
-            {
+            if (isset($this->activeGroups[$deviceId])) {
                 $this->activeGroups[$deviceId] = array_unique($this->activeGroups[$deviceId]);
             }
         }
-
     }
 
-    /**
-     * removes everything that is not part of the active
-     * groups array
-     */
     private function removeInactiveSlaveGroups(): void
     {
 
-        if($this->activeProbes === null){
+        if ($this->activeProbes === null) {
             return;
         }
 
         $this->setActiveSlaveGroups();
 
+        foreach ($this->activeProbes as $device => $probes) {
 
-        foreach ($this->activeProbes as $device => $probes){
-
-            if(!isset($this->activeGroups[$device])){
+            if (!isset($this->activeGroups[$device])) {
                 continue;
             }
 
-            foreach ($probes as $probe){
+            foreach ($probes as $probe) {
                 $path = $this->path . '/' . $device . '/' . $probe;
 
                 $storedSlaves = $this->storage->listItems($path);
 
-                if($storedSlaves === null){
+                if ($storedSlaves === null) {
                     continue;
                 }
 
                 $difference = array_diff($storedSlaves, $this->activeGroups[$device]);
 
-                if(empty($difference)){
+                if (empty($difference)) {
                     continue;
                 }
 
                 $this->storage->remove($difference, $path);
-
             }
-
         }
-
     }
 
-    /**
-     * collect the probes from active devices in storage
-     */
     private function setActiveProbes(): void
     {
-        if($this->storedActiveDevices === null){
+        if ($this->storedActiveDevices === null) {
             return;
         }
 
@@ -216,98 +180,66 @@ class CleanupService
             $deviceId = $device->getId();
             $probes = $device->getActiveProbes();
 
-            foreach ($probes as $probe){
+            foreach ($probes as $probe) {
                 $this->activeProbes[$deviceId][] = $probe->getid();
             }
 
-            if (isset($this->activeProbes[$deviceId]))
-            {
+            if (isset($this->activeProbes[$deviceId])) {
                 $this->activeProbes[$deviceId] = array_unique($this->activeProbes[$deviceId]);
             }
-
         }
-
     }
 
-    /**
-     * remove all inactive probes that are not part
-     * of the active probes array
-     */
     private function removeInactiveProbes(): void
     {
-
         $this->setActiveProbes();
 
-        if($this->activeProbes === null){
+        if ($this->activeProbes === null) {
             return;
         }
 
-
         foreach ($this->activeProbes as $device => $probes) {
-
             $path = $this->path . '/' . $device;
             $storedProbes = $this->storage->listItems($path);
-            if($storedProbes === null){
+            if ($storedProbes === null) {
                 continue;
             }
 
             $inactiveProbes = array_diff($storedProbes, $this->activeProbes[$device]);
 
-            if(empty($inactiveProbes))
-            {
+            if (empty($inactiveProbes)) {
                 continue;
             }
 
             $this->storage->remove($inactiveProbes, $path);
-
         }
-
     }
 
-    /**
-     * remove inactive devices
-     */
     private function removeInactiveDevices(): void
     {
-
         $nrInActive = $this->getInactiveDeviceCount();
 
-        if($nrInActive === 0){
+        if ($nrInActive === 0) {
             return;
         }
 
-        echo 'Removing ' . $nrInActive . ' devices...' . PHP_EOL;
-
         $items = array_chunk($this->inactiveDevices, 100);
 
-        foreach ($items as $key => $value){
-
+        foreach ($items as $key => $value) {
             $this->storage->remove($value, $this->path);
-
         }
-
     }
 
-    /**
-     * @return int
-     */
     public function getStoredDeviceCount() : int{
         return \count($this->storedDeviceIds);
     }
 
-    /**
-     * @return int
-     */
     public function getActiveDeviceCount() : int{
         return \count($this->activeDeviceIds);
     }
 
-    /**
-     * @return int
-     */
-    public function getInactiveDeviceCount() : int{
-        return \count($this->inactiveDevices);
+    public function getInactiveDeviceCount(): int
+    {
+        return count($this->inactiveDevices);
     }
-
-
 }
