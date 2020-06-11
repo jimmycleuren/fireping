1db27c85d94235c4d12f34fde00d9e63bbae6959
Jimmy Cleuren <jimmy.cleuren@gmail.com>
2020-06-11 22:00:39 +0200

    phpstan fixes on datafixtures (#230)

--- src/DataFixtures/ORM/ProbeArchiveFixtures.php
+++ src/DataFixtures/ORM/ProbeArchiveFixtures.php
@@ -12,7 +12,7 @@ use App\Entity\Probe;
 use App\Entity\ProbeArchive;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 
 class ProbeArchiveFixtures extends Fixture implements DependentFixtureInterface
 {
