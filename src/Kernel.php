d7cef43e3beeacd2db9f2b47bf1b665fca7a2bba
Kevin R <22906111+kevinreniers@users.noreply.github.com>
2020-06-19 15:39:55 +0200

    Resolve deprecations and use latest flex recipes. (#273)
    * Use latest symfony/framework-bundle recipe
    
    This resolves a number of deprecations related to outdated Kernel functionality.
    
    * Use latest doctrine/doctrine-bundle recipe
    
    This requires master installs to explicitly add the serverVersion information to their DATABASE_URL. The install docs already mention that this is required.
    
    I can pre-emptively update this on the fireping master to mariadb-10.1.26.
    
    However, running database installs shouldn't be affected anyway as doctrine will try to guess the server version anyway.
    
    * Force install new symfony/routes recipe.
    
    * Force install new symfony/security-bundle recipe.
    
    * Force install new symfony/security-bundle recipe.
    
    * Force install new monolog recipe.
    
    * Pin doctrine/inflector to <1.4
    
    This is necessary to remove hundreds of deprecation notices being spammed into our log files continuously. This is a recent deprecation that won't be fixed until doctrine/orm:2.8.
    
    After we've upgraded to doctrine/orm:2.8, we can safely remove this as a root requirement again.
    
    I've already created an issue for this to remind us when the time comes.
    
    Loading the index page now shows about 200 fewer deprecations.
    
    * Regenerate composer.json, composer.lock after merge
    
    * Trigger Travis rebuild...
    
    Co-authored-by: Kevin R <22906111+cgkkevinr@users.noreply.github.com>

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,69 +5,40 @@ namespace App;
 use App\DependencyInjection\Compiler\CommandPass;
 use App\ShellCommand\CommandInterface;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Routing\RouteCollectionBuilder;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
-    private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
-
-    public function getCacheDir()
+    protected function configureContainer(ContainerConfigurator $container): void
     {
-        if (isset($_ENV['CACHE_PATH']) && trim($_ENV['CACHE_PATH']) != "") {
-            return $_ENV['CACHE_PATH'] . '/' . $this->environment;
-        }
-        return $this->getProjectDir() . '/var/cache/' . $this->environment;
-    }
+        $container->import('../config/{packages}/*.yaml');
+        $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
 
-    public function getLogDir()
-    {
-        if (isset($_ENV['LOG_PATH']) && trim($_ENV['LOG_PATH']) != "") {
-            return $_ENV['LOG_PATH'];
-        }
-        return $this->getProjectDir() . '/var/log';
-    }
-
-    public function registerBundles(): iterable
-    {
-        $contents = require $this->getProjectDir().'/config/bundles.php';
-        foreach ($contents as $class => $envs) {
-            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
-                yield new $class();
-            }
+        if (file_exists(\dirname(__DIR__).'/config/services.yaml')) {
+            $container->import('../config/{services}.yaml');
+            $container->import('../config/{services}_'.$this->environment.'.yaml');
+        } else {
+            $path = \dirname(__DIR__).'/config/services.php';
+            (require $path)($container->withPath($path), $this);
         }
     }
 
-    public function getProjectDir(): string
+    protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        return \dirname(__DIR__);
-    }
+        $routes->import('../config/{routes}/'.$this->environment.'/*.yaml');
+        $routes->import('../config/{routes}/*.yaml');
 
-    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
-    {
-        $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
-        $container->setParameter('container.dumper.inline_class_loader', \PHP_VERSION_ID < 70400 || $this->debug);
-        $container->setParameter('container.dumper.inline_factories', true);
-        $confDir = $this->getProjectDir().'/config';
-
-        $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{packages}/'.$this->environment.'/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
-    }
-
-    protected function configureRoutes(RouteCollectionBuilder $routes): void
-    {
-        $confDir = $this->getProjectDir().'/config';
-
-        $routes->import($confDir.'/{routes}/'.$this->environment.'/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
+        if (file_exists(\dirname(__DIR__).'/config/routes.yaml')) {
+            $routes->import('../config/{routes}.yaml');
+        } else {
+            $path = \dirname(__DIR__).'/config/routes.php';
+            (require $path)($routes->withPath($path), $this);
+        }
     }
 
     protected function build(ContainerBuilder $container)
