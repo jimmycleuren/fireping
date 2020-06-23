a2a7fa94db7acabdb74774fbb86ec19c287a686e
Kevin R <22906111+kevinreniers@users.noreply.github.com>
2020-06-23 22:45:01 +0200

    Remove all "Created by PhpStorm." file docblocks. (#298)
    We don't do them consistently and they don't really add value anyway.
    
    Co-authored-by: Kevin R <22906111+cgkkevinr@users.noreply.github.com>
    Co-authored-by: Jimmy Cleuren <jimmy.cleuren@gmail.com>

--- src/AlertDestination/AlertDestinationInterface.php
+++ src/AlertDestination/AlertDestinationInterface.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jimmyc
- * Date: 8/03/2018
- * Time: 19:52
- */
 
 namespace App\AlertDestination;
 
--- src/OutputFormatter/TracerouteOutputFormatter.php
+++ src/OutputFormatter/TracerouteOutputFormatter.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kevinr
- * Date: 20/06/2017
- * Time: 9:40
- */
 
 namespace App\OutputFormatter;
 
