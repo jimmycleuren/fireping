1546f875fdafef159c30d0ff7e559965e72a51e3
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2020-06-24 23:08:45 +0200

    Add description to the generated migration.

--- src/Migrations/Version20200624202240.php
+++ src/Migrations/Version20200624202240.php
@@ -14,7 +14,7 @@ final class Version20200624202240 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return '';
+        return 'Add uniqueness constraint on domain parent/name combinations.';
     }
 
     public function up(Schema $schema) : void
