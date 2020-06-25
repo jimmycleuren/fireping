9db4c4d820c349b35fa0b7ac36fccfda3a4dd304
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2020-06-25 11:10:25 +0200

    Rename the class to match filename.

--- src/Migrations/Version20200624173840.php
+++ src/Migrations/Version20200624173840.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200624173845 extends AbstractMigration
+final class Version20200624173840 extends AbstractMigration
 {
     public function getDescription(): string
     {
