d9f56d53f80a85e6a52c66359ba1456646a46ef7
Kevin R <22906111+kevinreniers@users.noreply.github.com>
2020-06-25 13:46:30 +0200

    Add a unique constraint to names within parents. (#316)
    * Add a unique constraint to names within parents.
    
    This adds two things:
    
    - a database constraint preventing duplicate name entries within the same parent domain
    - an application-level validation constraint that will generate error messages for attempts to do so.
    
    Note that because for MySQL, two NULL values are -not- equal, this does not prevent multiple root domains with the same name on a database level (i.e. because of a race condition). We could avoid this by setting the parent_id column to NOT NULL and instead assuming the value 0 or -1 is "root domain".
    
    That way, a created index would be 0-name or -1-name, which can constrained for uniqueness.
    
    This closes #32 and #37.
    
    * Add description to the generated migration.
    
    Co-authored-by: Kevin R <22906111+cgkkevinr@users.noreply.github.com>
    Co-authored-by: Jimmy Cleuren <jimmy.cleuren@gmail.com>

---
+++ src/Migrations/Version20200624202240.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200624202240 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Add uniqueness constraint on domain parent/name combinations.';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE UNIQUE INDEX unique_name_in_parent ON domain (parent_id, name)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX unique_name_in_parent ON domain');
+    }
+}
