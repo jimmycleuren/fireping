71725b758a63787a5ec3a627d6b868786c6c3c85
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2020-06-26 12:58:45 +0200

    Add some default settings to HttpArguments.php

--- src/HttpArguments.php
+++ src/HttpArguments.php
@@ -5,13 +5,28 @@ namespace App;
 
 class HttpArguments extends ProbeArguments
 {
+    /**
+     * @var bool
+     */
+    private $allowRedirectsEnabled;
+    /**
+     * @var array
+     */
+    private $allowRedirectOptions;
+
+    public function __construct(bool $allowRedirectsEnabled, array $allowRedirectOptions)
+    {
+        $this->allowRedirectsEnabled = $allowRedirectsEnabled;
+        $this->allowRedirectOptions = $allowRedirectOptions;
+    }
+
     public static function fromJsonString(string $json): ProbeArgumentsInterface
     {
-        return new self();
+        return new self(true, ['someArg' => 'ok']);
     }
 
     public function asArray(): array
     {
-        return [];
+        return ['allowRedirectsEnabled' => $this->allowRedirectsEnabled, 'allowRedirectOptions' => $this->allowRedirectOptions];
     }
 }
