dddd96db4d49f634650f6c3a15b9124c73aab8fc
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2020-06-26 10:51:42 +0200

    Add HttpArgumentsType.php and TracerouteArgumentsType.php

---
+++ src/Form/Type/TracerouteArgumentsType.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class TracerouteArgumentsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        // Construct your arguments form here.
+    }
+}
