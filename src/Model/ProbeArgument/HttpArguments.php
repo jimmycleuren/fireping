eb80e4f247ffb08cbebe102fcbb09e3148487ffd
Kevin R <22906111+kevinreniers@users.noreply.github.com>
2020-06-30 11:06:29 +0200

    Form/probe/better argument uis (#321)
    * Add domain model PingArguments for arguments.
    
    * Add PingArgumentsType form type model
    
    * Add custom field rendering for probe arguments.
    
    * Use ProbeArgumentsField.php for arguments instead of TextareaField
    
    Also use a custom theme for the index and detail views.
    
    * Add a PingArgumentsField
    
    * Add interface and abstract implementation for ProbeArguments.php
    
    This allows us to move the ArrayAccess implementation to just the abstract class.
    
    * Extend PingArguments from ProbeArguments
    
    * Add initial implementation for Http/TracerouteArguments
    
    * Replace PingArguments with ProbeArgumentsInterface in Probe.php
    
    * Add HttpArgumentsType.php and TracerouteArgumentsType.php
    
    * Rename ping_arguments.html.twig to probe_arguments.twig
    
    * Remove arguments from search fields
    
    * Add ProbeArgumentsType.php
    
    This will function as the parent for all implementing classes so that we can assign a general blockPrefix to it.
    
    * Do not set the FormType on a ProbeArgumentsField.php
    
    We need to override this after fields were initialized but before the form gets rendered.
    
    * Remove Arguments from the Probe "new" view.
    
    We can't dynamically determine the type at that point yet.
    
    * Set the parent to ProbeArgumentsType on all ArgumentsTypes.
    
    * Change ping_arguments_row to probe_arguments_row
    
    This works now because we set the parent, which explicitly defines the block prefix as "probe_arguments".
    
    * Add default type ping and arguments {}
    
    * Add some default settings to HttpArguments.php
    
    * Suffix probe_arguments.twig as probe_arguments.html.twig
    
    * Render as an int field
    
    NumberType renders as a text field (that specializes in number handling).
    
    * Remove setArguments call in Fixtures
    
    They're not setting any explicit arguments, so the default value works fine.
    
    * Modify the arguments type prior to form creation.
    
    * Add NullArguments.php
    
    This represents "nothing" which is used in cases where the type of the probe is unrecognized.
    
    Using a NullObject in this context means we don't have to throw an exception if we don't recognize the given type.
    
    * Return NullArguments when type is unrecognized.
    
    * Don't test with dirty fixtures.
    
    * Create a default ProbeArgumentsType when type is unrecognized
    
    * Use asArray function instead of manually decoding the arguments.
    
    * Add type dummy back to dummy probe.
    
    This was being used in some tests and doesn't need to be not-dummy anymore since reworking some other bits of the code.
    
    * Move Arguments to their own Model\ProbeArgument namespace.
    
    * Make *Arguments constructors private
    
    * Remove test variables from HttpArguments.php
    
    * Remove unnecessary children to HttpArgumentsType.php
    
    * Add tests for *Arguments classes.
    
    * Don't use NullArguments constructor
    
    * Apply PHP-CS-Fixer fixes
    
    * added http probe arguments
    
    Co-authored-by: Kevin R <22906111+cgkkevinr@users.noreply.github.com>
    Co-authored-by: jimmycleuren <jimmy.cleuren@gmail.com>

---
+++ src/Model/ProbeArgument/HttpArguments.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\ProbeArgument;
+
+class HttpArguments extends ProbeArguments
+{
+    /**
+     * @var string|null
+     */
+    protected $host;
+    /**
+     * @var string|null
+     */
+    protected $path;
+    /**
+     * @var string|null
+     */
+    protected $protocol;
+
+    private function __construct(?string $host, ?string $path, ?string $protocol)
+    {
+        $this->host = $host;
+        $this->path = $path;
+        $this->protocol = $protocol;
+    }
+
+    public static function fromJsonString(string $json): ProbeArgumentsInterface
+    {
+        $data = json_decode($json, true, 512, JSON_THROW_ON_ERROR);
+        return new self($data['host'] ?? null, $data['path'] ?? null, $data['protocol'] ?? null);
+    }
+
+    public function asArray(): array
+    {
+        return [
+            'host' => $this->host,
+            'path' => $this->path,
+            'protocol' => $this->protocol
+        ];
+    }
+}
