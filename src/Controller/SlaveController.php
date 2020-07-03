30b2e6bc64165b2cd7faf32d6ea8655dd6f09c07
Jimmy Cleuren <jimmy.cleuren@gmail.com>
2020-07-03 13:13:09 +0200

    use incoming ip on master for slaves, fixes #341 (#342)
    Co-authored-by: Kevin R <22906111+kevinreniers@users.noreply.github.com>

--- src/Controller/SlaveController.php
+++ src/Controller/SlaveController.php
@@ -108,6 +108,7 @@ class SlaveController extends AbstractController
         }
 
         $slave->setLastContact(new \DateTime());
+        $slave->setIp($request->getClientIp());
         $entityManager->persist($slave);
         $entityManager->flush();
 
@@ -300,7 +301,6 @@ class SlaveController extends AbstractController
     {
         $data = json_decode($request->getContent());
 
-        $slave->setIp($data->ip);
         $slave->setLastContact(new \DateTime());
         $entityManager->persist($slave);
         $entityManager->flush();
