ca4f6d0347f4cb47560868b3936fdab3c70422b1
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2020-07-04 22:02:25 +0200

    Remove DEFAULT '' from the migration
    Doctrine doesn't seem to recognize it very well and we already default it on the Entity anyway.

--- src/Migrations/Version20200703132152.php
+++ src/Migrations/Version20200703132152.php
@@ -20,7 +20,7 @@ final class Version20200703132152 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("ALTER TABLE slave ADD version VARCHAR(255) NOT NULL DEFAULT ''");
+        $this->addSql("ALTER TABLE slave ADD version VARCHAR(255) NOT NULL");
     }
 
     public function down(Schema $schema) : void
