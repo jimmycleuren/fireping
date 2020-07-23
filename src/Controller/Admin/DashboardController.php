4146cdcec9fffe723eaeae4708900a36f13d0c90
kevinr <22906111+kevinreniers@users.noreply.github.com>
2020-07-23 14:04:16 +0200

    Add specific AlertDestinations to EA Dashboard.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,7 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Alert;
-use App\Entity\AlertDestination\AlertDestination;
+use App\Entity\AlertDestination\Email;
+use App\Entity\AlertDestination\Logging;
+use App\Entity\AlertDestination\Slack;
+use App\Entity\AlertDestination\Webhook;
 use App\Entity\AlertRule;
 use App\Entity\Device;
 use App\Entity\Domain;
@@ -37,12 +40,17 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Device', 'fas fa-folder-open', Device::class);
         yield MenuItem::linkToCrud('Alert', 'fas fa-folder-open', Alert::class);
         yield MenuItem::linkToCrud('AlertRule', 'fas fa-folder-open', AlertRule::class);
-        yield MenuItem::linkToCrud('AlertDestination', 'fas fa-folder-open', AlertDestination::class);
         yield MenuItem::linkToCrud('Probe', 'fas fa-folder-open', Probe::class);
         yield MenuItem::linkToCrud('ProbeArchive', 'fas fa-folder-open', ProbeArchive::class);
         yield MenuItem::linkToCrud('Slave', 'fas fa-folder-open', Slave::class);
         yield MenuItem::linkToCrud('SlaveGroup', 'fas fa-folder-open', SlaveGroup::class);
         yield MenuItem::linkToCrud('StorageNode', 'fas fa-folder-open', StorageNode::class);
         yield MenuItem::linkToCrud('User', 'fas fa-folder-open', User::class);
+
+        yield MenuItem::section('Alert Destinations');
+        yield MenuItem::linkToCrud('Slack', 'fas fa-folder-open', Slack::class);
+        yield MenuItem::linkToCrud('Email', 'fas fa-folder-open', Email::class);
+        yield MenuItem::linkToCrud('Webhook', 'fas fa-folder-open', Webhook::class);
+        yield MenuItem::linkToCrud('Logging', 'fas fa-folder-open', Logging::class);
     }
 }
