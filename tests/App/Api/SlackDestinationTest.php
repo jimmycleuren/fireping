dbb211f2d0c42b9fe058d73a4ebb9f7debfefd10
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2020-07-25 02:14:20 +0200

    Add more tests

--- tests/App/Api/SlackDestinationTest.php
+++ tests/App/Api/SlackDestinationTest.php
@@ -16,11 +16,22 @@ class SlackDestinationTest extends AbstractApiTest
         self::assertJson($this->client->getResponse()->getContent());
     }
 
+    public function testGetResource(): void
+    {
+        $repository = new AlertDestinationRepository($this->client->getContainer()->get('doctrine'));
+        $destination = $repository->findOneBy(['name' => 'slack']);
+
+        $this->client->request('GET', "/api/slack_destinations/{$destination->getId()}.json");
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertJson($this->client->getResponse()->getContent());
+    }
+
     public function testSuccessfulCreate(): void
     {
         $this->client->request('POST', '/api/slack_destinations.json', [], [], ['CONTENT_TYPE' => 'application/json'], json_encode([
-            'name'    => 'slack_create_test',
-            'url'     => 'https://slack.example',
+            'name' => 'slack_create_test',
+            'url' => 'https://slack.example',
             'channel' => 'channel'
         ], JSON_THROW_ON_ERROR));
 
@@ -30,22 +41,88 @@ class SlackDestinationTest extends AbstractApiTest
 
     public function testSuccessfulPatch(): void
     {
-        $this->client->request('POST', '/api/slack_destinations.json', [], [], ['CONTENT_TYPE' => 'application/json'], json_encode([
-            'name'    => 'slack_create_test',
-            'url'     => 'https://slack.example',
-            'channel' => 'channel'
-        ], JSON_THROW_ON_ERROR));
+        $repository = new AlertDestinationRepository($this->client->getContainer()->get('doctrine'));
+        $destination = $repository->findOneBy(['name' => 'slack']);
 
-        self::assertResponseStatusCodeSame(201);
+        self::assertSame('general', $destination->getChannel());
+
+        $this->client->request('PATCH', "/api/slack_destinations/{$destination->getId()}.json", [], [], ['CONTENT_TYPE' => 'application/merge-patch+json'], json_encode([
+            'channel' => 'BetterChannel'
+        ]));
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertJson($this->client->getResponse()->getContent());
+    }
+
+    public function testSuccessfulPut(): void
+    {
+        $repository = new AlertDestinationRepository($this->client->getContainer()->get('doctrine'));
+        $destination = $repository->findOneBy(['name' => 'slack']);
+
+        self::assertSame('general', $destination->getChannel());
+
+        $this->client->request('PUT', "/api/slack_destinations/{$destination->getId()}.json", [], [], ['CONTENT_TYPE' => 'application/json'], json_encode([
+            'channel' => 'BetterChannel',
+            'url' => 'https://slack.example'
+        ]));
+
+        self::assertResponseStatusCodeSame(200);
         self::assertJson($this->client->getResponse()->getContent());
     }
 
     public function testSuccessfulRemove(): void
     {
-        $alertDestinationRepository = new AlertDestinationRepository($this->client->getContainer()->get('doctrine'));
-        $id = $alertDestinationRepository->findOneBy(['name' => 'unused-slack'])->getId();
+        $repository = new AlertDestinationRepository($this->client->getContainer()->get('doctrine'));
+        $id = $repository->findOneBy(['name' => 'unused-slack'])->getId();
         $this->client->request('DELETE', "/api/slack_destinations/$id.json");
 
         self::assertResponseStatusCodeSame(204);
+    }
+
+    public function testRequiredParametersCannotBeBlank(): void
+    {
+        $this->client->request('POST', '/api/slack_destinations.json', [], [], ['CONTENT_TYPE' => 'application/json'], '');
+
+        self::assertResponseStatusCodeSame(400);
+        self::assertJson($this->client->getResponse()->getContent());
+        dump($this->client->getResponse()->getContent());
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+        dump($data);
+        self::assertArrayHasKey('violations', $data);
+        self::assertSame([
+            [
+                'propertyPath' => 'url',
+                'message' => 'This value should not be blank.'
+            ],
+            [
+                'propertyPath' => 'channel',
+                'message' => 'This value should not be blank.'
+            ],
+            [
+                'propertyPath' => 'name',
+                'message' => 'This value should not be blank.'
+            ]
+        ], $data['violations']);
+    }
+
+    public function testMustHaveValidUrl(): void
+    {
+        $this->client->request('POST', '/api/slack_destinations.json', [], [], ['CONTENT_TYPE' => 'application/json'], json_encode([
+            'name' => 'new-slack',
+            'url' => 'invalid url',
+            'channel' => 'channel',
+        ]));
+
+        self::assertResponseStatusCodeSame(400);
+        self::assertJson($this->client->getResponse()->getContent());
+
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+        self::assertArrayHasKey('violations', $data);
+        self::assertSame([
+            [
+                'propertyPath' => 'url',
+                'message' => 'This value is not a valid URL.'
+            ]
+        ], $data['violations']);
     }
 }
