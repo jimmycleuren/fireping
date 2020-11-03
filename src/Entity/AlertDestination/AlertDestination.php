736b03a23a44a7ce4cc74628e342145e7e9746da
jimmycleuren <jimmy.cleuren@gmail.com>
2020-11-03 22:06:57 +0100

    add missing file

---
+++ src/Entity/AlertDestination/AlertDestination.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Entity\AlertDestination;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * AlertDestination.
+ *
+ * @ORM\Table(name="alert_destination")
+ * @ORM\Entity(repositoryClass="App\Repository\AlertDestinationRepository")
+ * @ApiResource
+ * @ORM\Cache(usage="NONSTRICT_READ_WRITE")
+ */
+class AlertDestination
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     * @Assert\NotBlank
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     * @Assert\NotBlank
+     */
+    private $type;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="parameters", type="json", nullable=true)
+     */
+    private $parameters;
+
+    /**
+     * @return int
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): void
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return array
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * @param array $parameters
+     */
+    public function setParameters($parameters): void
+    {
+        $this->parameters = $parameters;
+    }
+
+    public function __toString(): ?string
+    {
+        return $this->name;
+    }
+}
