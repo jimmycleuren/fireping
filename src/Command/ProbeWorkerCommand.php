d92369955375cdae7fa79a8143efb3c678da07f5
Jimmy Cleuren <jimmy.cleuren@gmail.com>
2021-03-19 20:14:07 +0100

    fix error logging (#585)

--- src/Command/ProbeWorkerCommand.php
+++ src/Command/ProbeWorkerCommand.php
@@ -172,7 +172,7 @@ class ProbeWorkerCommand extends Command
                     $this->sendResponse('exception', 0, $errorMessage);
             }
         } catch (Exception $e) {
-            $errorMessage = sprintf('worker %d fatal: ' . $e->getMessage());
+            $errorMessage = sprintf('worker %s fatal: ' . $e->getMessage());
             $this->logger->error($errorMessage);
             $this->sendResponse('exception', 0, $errorMessage);
 
