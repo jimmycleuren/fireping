44217a05259fb3e40e8965839a2b8d027ac7751c
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2021-04-22 22:19:36 +0200

    Move App\OutputFormatter to App\Slave\OutputFormatter

--- tests/App/Slave/OutputFormatter/PingOutputFormatterTest.php
+++ tests/App/Slave/OutputFormatter/PingOutputFormatterTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Tests\App\OutputFormatter;
+namespace App\Tests\Slave\OutputFormatter;
 
 use App\OutputFormatter\PingOutputFormatter;
 use PHPUnit\Framework\TestCase;
