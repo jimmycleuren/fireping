683eab2368c0c0fde38f8f82603da9b89a64cff5
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2021-05-02 13:23:28 +0200

    Add helper method to clear the plain password on User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -138,8 +138,7 @@ class User implements UserInterface
      */
     public function eraseCredentials()
     {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
+        $this->plainPassword = null;
     }
 
     public function getEmail(): ?string
