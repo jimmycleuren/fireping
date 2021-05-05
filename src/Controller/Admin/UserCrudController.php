d58d57ea281142c64782c6a18a58e627f406d86d
Kevin Reniers <22906111+kevinreniers@users.noreply.github.com>
2021-05-05 09:26:11 +0200

    580 fix password reset easyadmin (#704)
    * Add helper method to clear the plain password on User
    
    * Encode passwords set through EasyAdmin
    
    * Remove password hashing code from UserCrudController.php
    
    * Make encodePassword public
    
    * Add null to var annotation of plainPassword property of User
    
    This makes PHPstan happy.
    
    Co-authored-by: Kevin R <22906111+cgkkevinr@users.noreply.github.com>
    Co-authored-by: Jimmy Cleuren <jimmy.cleuren@gmail.com>

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
@@ -12,32 +11,9 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
 
 class UserCrudController extends AbstractCrudController
 {
-    private $encoderFactory;
-
-    public function __construct(EncoderFactoryInterface $encoderFactory)
-    {
-        $this->encoderFactory = $encoderFactory;
-    }
-
-    public function hashPassword(User $user): void
-    {
-        $plainPassword = $user->getPlainPassword();
-
-        if ('' === $plainPassword || null === $plainPassword) {
-            return;
-        }
-
-        $encoder = $this->encoderFactory->getEncoder($user);
-
-        $hashedPassword = $encoder->encodePassword($plainPassword, $user->getSalt());
-        $user->setPassword($hashedPassword);
-        $user->eraseCredentials();
-    }
-
     public static function getEntityFqcn(): string
     {
         return User::class;
@@ -58,8 +34,8 @@ class UserCrudController extends AbstractCrudController
         $username = TextField::new('username');
         $email = TextField::new('email');
         $roles = ChoiceField::new('roles')
-                            ->setChoices(['ROLE_API' => 'ROLE_API', 'ROLE_ADMIN' => 'ROLE_ADMIN'])
-                            ->setFormTypeOption('multiple', true);
+            ->setChoices(['ROLE_API' => 'ROLE_API', 'ROLE_ADMIN' => 'ROLE_ADMIN'])
+            ->setFormTypeOption('multiple', true);
         $plainPassword = TextField::new('plainPassword')->setFormType(PasswordType::class);
         $id = IntegerField::new('id', 'ID');
         $enabled = BooleanField::new('enabled');
@@ -82,12 +58,5 @@ class UserCrudController extends AbstractCrudController
         }
 
         return [];
-    }
-
-    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
-    {
-        $this->hashPassword($entityInstance);
-        $entityManager->persist($entityInstance);
-        $entityManager->flush();
     }
 }
