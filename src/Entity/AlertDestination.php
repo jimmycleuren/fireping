136e9b281cde1f7dc34a2d2442b56f7990389bf4
Kevin Reniers <22906111+kevinreniers@users.noreply.github.com>
2021-05-12 00:00:02 +0200

    Require PHP 7.4 (#725)
    * Require PHP 7.4
    
    PHP 7.3 is no longer supported, so I think it's high time we promote our base requirement to PHP 7.4 instead.
    
    In 6 months time 7.4 will also leave active support, at which point we should make our minimal version 8.0.
    
    * update ci pipeline to use php version 7.4 and 8.0
    
    * allow php 8.0 in composer.json
    
    * updating some dependencies
    
    * php 8 compatibility
    
    * php 8 compatibility
    
    * fixing wrong namespace
    
    * fixing wrong namespace
    
    Co-authored-by: Kevin R <22906111+cgkkevinr@users.noreply.github.com>
    Co-authored-by: jimmycleuren <jimmy.cleuren@gmail.com>

--- src/Entity/AlertDestination.php
+++ src/Entity/AlertDestination.php
@@ -98,7 +98,7 @@ class AlertDestination
         $this->parameters = $parameters;
     }
 
-    public function __toString(): ?string
+    public function __toString(): string
     {
         return $this->name;
     }
