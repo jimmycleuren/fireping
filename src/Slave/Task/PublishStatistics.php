ac9121109e9f04fa76bd11a6a05b34dbe2034915
Kevin Reniers <22906111+kevinreniers@users.noreply.github.com>
2021-05-12 19:50:14 +0200

    Use Cookies on Slaves (#719)
    Co-authored-by: Kevin R <22906111+cgkkevinr@users.noreply.github.com>

--- src/Slave/Task/PublishStatistics.php
+++ src/Slave/Task/PublishStatistics.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace App\Slave\Task;
 
 use App\Slave\Client\FirepingClient;
+use GuzzleHttp\Cookie\FileCookieJar;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\RequestOptions;
 use Psr\Log\LoggerInterface;
 use stdClass;
 
@@ -35,7 +37,10 @@ class PublishStatistics implements TaskInterface
         $startedAt = microtime(true);
         $this->logger->info(sprintf('worker: publishing stats to master (%d bytes)', strlen(serialize($this->body))));
         try {
-            $response = $this->client->request($this->method, $this->endpoint, ['json' => $this->body]);
+            $response = $this->client->request($this->method, $this->endpoint, [
+                RequestOptions::JSON => $this->body,
+                RequestOptions::COOKIES => new FileCookieJar(sys_get_temp_dir().'/.slave.cookiejar.json', true)
+            ]);
 
             $this->logger->info(sprintf('worker: published stats (took %.2f seconds)', microtime(true) - $startedAt));
             return ['code' => $response->getStatusCode(), 'contents' => (string) $response->getBody()];
